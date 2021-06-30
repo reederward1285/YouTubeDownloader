890d9f8408874a6612601d75d57b79217a8e73c8
Reeder Ward II <54606752+reederward1285@users.noreply.github.com>
2021-06-30 18:09:27 +0200

    minor update

---
+++ YouTubeDownloader.bat
@@ -0,0 +1,1 @@
+python3 YouTubeDownloader/YouTubeDownloader.py
---
+++ install_YouTubeDownloader.bat
@@ -0,0 +1,17 @@
+:: install python
+cd src
+python-3.9.5-amd64.exe /passive InstallAllUsers=1 PrependPath=1 Include_test=0
+
+:: install pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+python -m pip install --upgrade pip
+
+:: install youtube downloader
+pip install youtube_dl
+
+:: install tkinter
+pip install tk
+
+:: run the program
+python3 YouTubeDownloader/YouTubeDownloader.py
